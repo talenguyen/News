a498de35e5678650ed9921f0c10543e2f5f935f5
Giang Nguyen <giang.nguyen@tiki.vn>
2019-02-08 22:57:22 +0700

    migrate to androidX

--- app/src/main/java/com/besimplify/news/features/articles/ArticlesFragment.kt
+++ app/src/main/java/com/besimplify/news/features/articles/ArticlesFragment.kt
@@ -2,10 +2,10 @@ package com.besimplify.news.features.articles
 
 import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v4.content.ContextCompat
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import androidx.fragment.app.Fragment
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -55,7 +55,7 @@ class ArticlesFragment : Fragment() {
       false
     ).apply {
       listNews = findViewById(R.id.list_news)
-      listNews.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+      listNews.layoutManager = LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
       listNews.adapter = newsAdapter
     }
   }
